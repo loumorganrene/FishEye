27e6f267edc90e1c0ee3f53b7ea58e4c2010fdcf
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-04-15 16:26:51 +0200

    Homepage: createElement + addClass on PhotographerCard template, css modif, html for accessibility WIP

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -11,12 +11,18 @@ class PhotographerCard {
         const h2 = document.createElement( 'h2' );
         h2.textContent = `${this._photographer.name}`;
         const location = document.createElement( 'p' );
+        location.classList.add("location");
         location.textContent = `${this._photographer.city}, ${this._photographer.country}`;
+        const tagline = document.createElement( 'p' );
+        tagline.classList.add("tagline");
+        tagline.textContent = `${this._photographer.tagline}`;
         const price = document.createElement( 'p' );
-        price.textContent = `${this._photographer.price}€`;
+        price.classList.add("price");
+        price.textContent = `${this._photographer.price}€/jour`;
         $wrapper.appendChild(img);
         $wrapper.appendChild(h2);
         $wrapper.appendChild(location);
+        $wrapper.appendChild(tagline);
         $wrapper.appendChild(price);
 
         return $wrapper
