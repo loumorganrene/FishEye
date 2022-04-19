b8c1eb2f0a4a252092a34e56f87ba6c45bd7a2aa
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-04-19 12:46:36 +0200

    Homepage: css refacto

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -7,17 +7,18 @@ class PhotographerCard {
         const $wrapper= document.createElement( 'article' );
 
         const img = document.createElement( 'img' );
-        img.setAttribute("src", `assets/photographers/portraits/${this._photographer.portrait}`);
+        img.setAttribute( "src", `assets/photographers/portraits/${this._photographer.portrait}` );
+        img.setAttribute( "alt", `${this._photographer.name}` );
         const h2 = document.createElement( 'h2' );
         h2.textContent = `${this._photographer.name}`;
         const location = document.createElement( 'p' );
-        location.classList.add("location");
+        location.classList.add( "location" );
         location.textContent = `${this._photographer.city}, ${this._photographer.country}`;
         const tagline = document.createElement( 'p' );
-        tagline.classList.add("tagline");
+        tagline.classList.add( "tagline" );
         tagline.textContent = `${this._photographer.tagline}`;
         const price = document.createElement( 'p' );
-        price.classList.add("price");
+        price.classList.add( "price" );
         price.textContent = `${this._photographer.price}€/jour`;
         $wrapper.appendChild(img);
         $wrapper.appendChild(h2);
