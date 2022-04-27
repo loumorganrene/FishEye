02eefcbe0ef9ef65748b7e32e1cfa7929752703f
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-04-27 17:32:25 +0200

    createElement figure with link, img+figcaption(h2) in link, div desc with p*3, role, aria-label and css class+focus

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -4,13 +4,27 @@ export class PhotographerCard {
     }
 
     createPhotographerCard() {
-        const $wrapper= document.createElement( 'article' );
+        const $wrapper = document.createElement( 'article' );
+        $wrapper.setAttribute( "aria-label",  `Carte de ${this._photographer.name}` );
 
+        const figure = document.createElement( 'figure' );
+        figure.classList.add( "photographer_pres" );
+        figure.setAttribute( "role",  "figure" );
+        figure.setAttribute( "aria-label",  `${this._photographer.name}` );
+        $wrapper.appendChild(figure);
+        const a = document.createElement( 'a' );
+        a.setAttribute( "href",  `` );
+        figure.appendChild(a);
         const img = document.createElement( 'img' );
         img.setAttribute( "src", `assets/photographers/portraits/${this._photographer.portrait}` );
-        img.setAttribute( "alt", `${this._photographer.name}` );
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute( "alt", `` );
+        const h2 = document.createElement( 'figcaption' );
         h2.textContent = `${this._photographer.name}`;
+        h2.setAttribute( "id",  `${this._photographer.id}` );
+
+        const desc = document.createElement( 'div' );
+        desc.classList.add( "photographer_desc" );
+        $wrapper.appendChild(desc);
         const location = document.createElement( 'p' );
         location.classList.add( "location" );
         location.textContent = `${this._photographer.city}, ${this._photographer.country}`;
@@ -20,11 +34,11 @@ export class PhotographerCard {
         const price = document.createElement( 'p' );
         price.classList.add( "price" );
         price.textContent = `${this._photographer.price}€/jour`;
-        $wrapper.appendChild(img);
-        $wrapper.appendChild(h2);
-        $wrapper.appendChild(location);
-        $wrapper.appendChild(tagline);
-        $wrapper.appendChild(price);
+        a.appendChild(img);
+        a.appendChild(h2);
+        desc.appendChild(location);
+        desc.appendChild(tagline);
+        desc.appendChild(price);
 
         return $wrapper
     }
