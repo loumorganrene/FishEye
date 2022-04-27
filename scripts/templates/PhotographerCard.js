bee17208e623c0fc95ba7538308a9eed882618e3
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-04-27 22:48:19 +0200

    set the url param for each card on href by id + attempt of documentFragment

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -4,42 +4,65 @@ export class PhotographerCard {
     }
 
     createPhotographerCard() {
+        const docFrag = document.createDocumentFragment();
         const $wrapper = document.createElement( 'article' );
-        $wrapper.setAttribute( "aria-label",  `Carte de ${this._photographer.name}` );
 
+        // First section of a photographer card
         const figure = document.createElement( 'figure' );
         figure.classList.add( "photographer_pres" );
         figure.setAttribute( "role",  "figure" );
-        figure.setAttribute( "aria-label",  `${this._photographer.name}` );
-        $wrapper.appendChild(figure);
+
+        // Link to photographer page
         const a = document.createElement( 'a' );
-        a.setAttribute( "href",  `` );
+        a.setAttribute( "href",  `photographer.html?id=${this._photographer.id}` );
+        a.setAttribute( "aria-label",  `Visiter la page de ${this._photographer.name}` );
         figure.appendChild(a);
+
+        // Photographer portrait
         const img = document.createElement( 'img' );
         img.setAttribute( "src", `assets/photographers/portraits/${this._photographer.portrait}` );
-        img.setAttribute( "alt", `` );
+        img.setAttribute( "alt", `Portrait de ${this._photographer.name}` );
+
+        // Photographer name
         const h2 = document.createElement( 'figcaption' );
         h2.textContent = `${this._photographer.name}`;
         h2.setAttribute( "id",  `${this._photographer.id}` );
 
+        // Push portrait & name in link
+        a.appendChild(img);
+        a.appendChild(h2);
+
+        // Seconde section of a photographer card
         const desc = document.createElement( 'div' );
         desc.classList.add( "photographer_desc" );
-        $wrapper.appendChild(desc);
+
+        // Photographer city & country
         const location = document.createElement( 'p' );
         location.classList.add( "location" );
         location.textContent = `${this._photographer.city}, ${this._photographer.country}`;
+
+        // Photographer quote
         const tagline = document.createElement( 'p' );
         tagline.classList.add( "tagline" );
         tagline.textContent = `${this._photographer.tagline}`;
+
+        // Photographer price per day
         const price = document.createElement( 'p' );
         price.classList.add( "price" );
         price.textContent = `${this._photographer.price}€/jour`;
-        a.appendChild(img);
-        a.appendChild(h2);
+
+        // Push portrait & name in link
         desc.appendChild(location);
         desc.appendChild(tagline);
         desc.appendChild(price);
 
-        return $wrapper
+        // Push first & second section into Docfragment
+        $wrapper.appendChild(figure);
+        $wrapper.appendChild(desc);
+
+        // Push DocfragmentElement into 
+        docFrag.appendChild($wrapper);
+
+        return docFrag;
     }
 }
