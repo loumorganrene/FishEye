460a975aec4222400b4de3d81361cc16e8dbdf40
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-04-30 12:43:07 +0200

    Starting: Contact modale creation html+css, keyboard focus+interactive on utils/contactForms.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,15 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+const closeIcon = document.getElementById("close_modal-icon");
+
+closeIcon.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      closeModal();
+    }
+  }); 
