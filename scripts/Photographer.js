8fcc6ea93647515853d59a9b70732e8583cf8548
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-05-01 14:36:25 +0200

    Done: contact modal form

--- scripts/Photographer.js
+++ scripts/Photographer.js
@@ -2,7 +2,7 @@ import { MediaApi, PhotographerApi } from "./api/Api.js";
 import { Photographer } from "./models/PhotographersModel.js";
 import { PhotographerBanner } from "./templates/PhotographerBanner.js";
 
-class App {
+export class App {
     constructor() {
         this.$bannerWrapper = document.querySelector('.photograph-header')
         this.photographersApi = new PhotographerApi('data/photographers.json')
@@ -16,15 +16,17 @@ class App {
         const url = new URL(window.location.href);
         const photographerPageId = url.searchParams.get('id')
         //Fetch photographer data by id
-        const photographerInfo = photographersData.find(photographerId)
+        const photographerApiId = photographersData.find(photographerId)
         function photographerId(photographer) {
             return photographer.id == photographerPageId
         }
         //Formatting photographer data
-        const photographer = new Photographer(photographerInfo)
+        const photographer = new Photographer(photographerApiId)
         //Applying template for the photogapher page banner
         const Template = new PhotographerBanner(photographer)
         this.$bannerWrapper.appendChild(Template.createPhotographerBanner())
+        //Add photographer name to the contact me title
+        document.getElementById('contact_me').innerHTML = `Contactez-moi <br> ${photographer.name}`
     }
 
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,23 +1,35 @@
 //DOM element selector
 const body = document.body;
-const modal = document.getElementById("contact_modal");
-const main = document.getElementById("main");
-const openBtn = document.querySelector("contact_button");
-const closeBtn = document.getElementById("close_modal-icon");
+const main = document.getElementById("main")
+const modal = document.getElementById("contact_modal")
+const form = document.querySelector("form")
+const formData = document.querySelectorAll(".formData")
+const openBtn = document.getElementById("open_btn")
+const closeIcon = document.getElementById("close_icon")
+const submitBtn = document.getElementById("submit_btn")
+
+// Form Elements
+const firstName = document.getElementById("first")
+const lastName = document.getElementById("last")
+const email = document.getElementById("email")
+const message = document.querySelector("textarea")
+
+// Regex
+const nameRegExp = /[^0-9<>()\[\]\\.,;:\s@"][A-Za-z]{1,}/
+const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))+$/
+
 //Event listener
-closeBtn.addEventListener("keydown", keyboardCloseModal); 
+closeIcon.addEventListener("keydown", keyboardCloseModal); 
 //Open contact modal
 function displayModal() {
-    body.style.overflow = "hidden";
     main.setAttribute('aria-hidden', 'true');
 	modal.style.display = "block";
     modal.setAttribute('aria-hidden', 'false');
-    closeBtn.focus();
+    closeIcon.focus();
 }
 //Close contact modal
 function closeModal() {
-    body.style.overflow = "scroll";
-    main.setAttribute('aria-hidden', 'true');
+    main.setAttribute('aria-hidden', 'false');
     modal.style.display = "none";
     modal.setAttribute('aria-hidden', 'true');
     openBtn.focus();
@@ -30,4 +42,88 @@ function keyboardCloseModal(event) {
         // Trigger the button element with a click
         closeModal();
       }
+}
+
+// verify firstname
+formData[0].addEventListener("input", firstNameIsValid); // verify entry on type
+function firstNameIsValid() {
+  if (!firstName.value.match(nameRegExp)) {
+    formData[0].setAttribute("data-error-visible", "true");
+    formData[0].setAttribute("data-error", "Entrez un prénom valide.");
+    return false;
+  } else {
+    formData[0].setAttribute("data-error-visible", "false");
+    formData[0].setAttribute("data-error", "");
+    return true;
+  }
+}
+
+// verify lastname
+formData[1].addEventListener("input", lastNameIsValid); // verify entry on type
+function lastNameIsValid() {
+  if (!lastName.value.match(nameRegExp)) {
+    formData[1].setAttribute("data-error-visible", "true");
+    formData[1].setAttribute("data-error", "Entrez un nom valide.");
+    return false;
+  } else {
+    formData[1].setAttribute("data-error-visible", "false");
+    formData[1].setAttribute("data-error", "");
+    return true;
+  }
+}
+
+// verify email
+formData[2].addEventListener("input", emailIsValid); // verify entry on type
+function emailIsValid() {
+  if (!email.value.match(emailRegExp)) {
+    formData[2].setAttribute("data-error-visible", "true");
+    formData[2].setAttribute("data-error", "Entrez un email valide.");
+    return false;
+  } else {
+    formData[2].setAttribute("data-error-visible", "false");
+    formData[2].setAttribute("data-error", "");
+    return true;
+  }
+}
+
+// verify email
+formData[3].addEventListener("input", messageIsValid); // verify entry on type
+function messageIsValid() {
+  if (message.value == "") {
+    formData[3].setAttribute("data-error-visible", "true");
+    formData[3].setAttribute("data-error", "Votre message ne peut-être vide.");
+    return false;
+  } else {
+    formData[3].setAttribute("data-error-visible", "false");
+    formData[3].setAttribute("data-error", "");
+    return true;
+  }
+}
+
+// validate modal form
+submitBtn.addEventListener("click", validate);
+
+// validate contact form
+function validate(e) {
+  // prevent closing modal & erasing user input data
+  e.preventDefault();
+  if (
+  firstNameIsValid() &&
+  lastNameIsValid() &&
+  emailIsValid() &&
+  messageIsValid()
+  ) { 
+    console.log(
+        "Prénom:"+firstName.value+
+        ", Nom:"+lastName.value+
+        ", Email:"+email.value+
+        ", Message:"+message.value);
+    form.reset();
+  } 
+  else {
+    firstNameIsValid(),
+    lastNameIsValid(),
+    emailIsValid(),
+    messageIsValid()
+  }
 }
